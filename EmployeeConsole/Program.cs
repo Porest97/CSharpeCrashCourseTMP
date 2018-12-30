d0adec0198f7a49a996ebc5be4a4f3e02bcbb347
Per Orest <per.orest@outlook.com>
2018-12-30 12:08:17 +0100

    Create Program.cs

---
+++ EmployeeConsole/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeConsole
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            EmployeeManager employeeManager = new EmployeeManager();
+            int menuSelection = -1;
+            while (menuSelection != 0)
+            {
+                Console.WriteLine("What do you wanna do? ");
+                Console.WriteLine("Press 0 to Exit");
+                Console.WriteLine("Press 1 to create Employee");
+                Console.WriteLine("Press 2 to show Employees");
+                
+                var valueSelectedAsString = Console.ReadLine();
+                if (int.TryParse(valueSelectedAsString, out menuSelection))
+                {
+                    switch (menuSelection)
+                    {
+                        case 0:
+                            Console.WriteLine("Bye bye");
+                            break;
+                        case 1:
+                            Console.WriteLine("Type Email:");
+                            var email = Console.ReadLine();
+                            Console.WriteLine(employeeManager.CreateEmployee(email));
+                            break;
+                        case 2:
+                            employeeManager.PrintAllEmployees();
+                            break;
+                        case 3:
+                            //
+                            break;
+                        default:
+                            Console.WriteLine("Not a valid Number, please try again ");
+                            break;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Not a Number, please try again ");
+                    menuSelection = -1;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
