a179e666f49627c8f46d6ad1071b09191196b67a
Per Orest <per.orest@outlook.com>
2018-12-30 12:06:09 +0100

    AllApps
    Pelle Orest

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = InputUtils.GetInputNumber("Type your first number");
+
+            int secondNumber = InputUtils.GetInputNumber("Type your second number");
+
+            Console.WriteLine("You wrote the numbers: " + firstNumber + " and " + secondNumber + " addaed it results in " + (firstNumber + secondNumber));
+
+            Console.ReadLine();
+
+        }
+
+       
+    }
+}
---
+++ ConsoleCar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarConsole.Model;
+
+namespace CarConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car theCar = new Car()
+            {
+                Color = "Seashell",
+                LicenceNumber = "NTB 441",
+                Make = "Volvo",
+                Year = 2010
+            };
+           
+            
+            
+            // Console.WriteLine("___________________________");
+
+            Console.WriteLine(theCar.ToString());
+           
+
+            //Console.WriteLine("____________________________");
+           
+            theCar.Drive();
+            theCar.Break();
+            theCar.Drive();
+            theCar.Break();
+            Console.WriteLine("==================================================");
+            Console.WriteLine("---------------------->Booom<---------------------");
+            Console.WriteLine("------------------>We are done !<---------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
